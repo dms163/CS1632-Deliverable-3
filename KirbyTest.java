f780a8f9ac4acd7db845534d7805e1d5f39e0e58
Therese Dachille <tcd12@pitt.edu>
2016-03-03 00:13:16 -0500

    user story 1 completed

--- KirbyTest.java
+++ KirbyTest.java
@@ -11,68 +11,73 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class KirbyTest {
-  static WebDriver driver = new FirefoxDriver();
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
+  static WebDriver driver = new HtmlUnitDriver();
 
+  //start at the homepage for each WiKirby test
   @Before
   public void setUp() throws Exception {
     //driver = new HtmlUnitDriver();
     driver.get("http://wikirby.com/wiki/Kirby_Wiki");
     
   }
-
- //User Scenario:
-  //Given on WiKirby homepage,
-  //When I click on the 'Enemies' link,
-  //Then I should be taken to the 'Category: Enemies' page
+  
+////////////////////////////////////////////////////////////////
+  /*User Story 1:
+  As a user, I would like to browse Kirby's different types of enemies,
+  so that I can learn about Kirby's enemies.
+  */
+ ///////////////////////////////////////////////////////////////
+  
+ /*Scenario 1-1:
+  *Given on WiKirby homepage,
+  *When I click on the 'Enemies' link,
+  *Then I should be taken to the 'Category: Enemies' page
+  */
   @Test
   public void testEnemies() throws Exception {
-	driver.get("http://wikirby.com/wiki/Kirby_Wiki");
-    driver.findElement(By.cssSelector("a[title=\"Visit the main page\"]")).click();
     driver.findElement(By.linkText("Enemies")).click();
     assertEquals("Category:Enemies - WiKirby", driver.getTitle());
   }
-
-  @After
-  public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
+  
+  /*Scenario 1-2:
+   *Given on WiKirby 'Enemies' page,
+   *When I click on the 'Horror Tramp' link,
+   *Then I should be taken to the 'WiKirby Horror Tramp' page
+   */
+  @Test
+  public void testHorrorTramp() throws Exception {
+    driver.get("http://wikirby.com/wiki/Category:Enemies");
+    driver.findElement(By.linkText("Horror Tramp")).click();
+    String newPageTitle = driver.getTitle();
+	assertTrue(newPageTitle.contains("Horror Tramp"));
   }
-
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
+  
+  /*Scenario 1-3:
+   *Given on WiKirby 'Enemies' page,
+   *When I click on the 'Broom Hatter' link,
+   *Then I should be taken to the 'WiKirby Broom Hatter' page
+   */
+  @Test
+  public void testBroomHatter() throws Exception {
+	    driver.get("http://wikirby.com/wiki/Category:Enemies");
+	    driver.findElement(By.linkText("Broom Hatter")).click();
+	    String newPageTitle = driver.getTitle();
+		assertTrue(newPageTitle.contains("Broom Hatter"));
+	  }
+  
+  /*Scenario 1-4:
+   *Given on WiKirby 'Enemies' page,
+   *When I click on the 'Waddle Doo' link,
+   *Then I should not be taken to the 'WiKirby Waddle Dee' page
+   */
+  @Test
+  public void testWaddleDoo() throws Exception {
+    driver.get("http://wikirby.com/wiki/Category:Enemies");
+    driver.findElement(By.linkText("Waddle Doo")).click();
+    String newPageTitle = driver.getTitle();
+	assertTrue(!newPageTitle.contains("Waddle Dee"));
   }
+  
+  
 
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
-
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
 }
